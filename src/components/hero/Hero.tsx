604bd709883a29e868b908be5c8630a8a0c40222
snowwolf <engakinkunmi@gmail.com>
2026-07-09 16:19:00 +0100

    fix: remove erroneous w-screen class from layout body and apply to hero section instead

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
   return (
     <section
       id="home"
-      className="relative  min-h-screen flex items-center overflow-hidden text-foreground"
+      className="relative w-screen min-h-screen flex items-center overflow-hidden text-foreground"
     >
       <Container>
         <div className="grid items-center gap-16 lg:grid-cols-2 mt-32 ">
