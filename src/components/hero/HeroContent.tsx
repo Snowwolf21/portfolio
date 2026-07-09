e0e1c070804dc6d6471a61dee04a635ebc8dc880
snowwolf <engakinkunmi@gmail.com>
2026-07-09 16:37:27 +0100

    style: adjust Hero section text alignment, font sizes, and spacing for better responsive layout

--- src/components/hero/HeroContent.tsx
+++ src/components/hero/HeroContent.tsx
@@ -4,21 +4,21 @@ import HeroSocials from "./HeroSocials";
 
 export default function HeroContent() {
   return (
-    <div className="space-y-8 text-center md:text-left">
+    <div className="space-y-8 text-left">
       {/* Pill Badge */}
       <span className="inline-flex rounded-full border border-accent/30 bg-accent/10 px-4.5 py-1.5 text-xs font-bold uppercase tracking-widest text-foreground/60">
         Hello there,👋🏽
       </span>
 
-      <div className="space-y-4">
+      <div className="space-y-4 text-center md:text-left">
         {/* Main Header */}
-        <h1 className="text-5xl font-black leading-none md:text-7xl tracking-tight text-foreground/60">
+        <h1 className="text-4xl font-black leading-none md:text-7xl tracking-tight text-foreground/60 ">
           I&apos;m{" "}
           <span className="text-foreground/60">
             Akinkunmi
           </span>
           <br className="hidden sm:inline" />
-          <span className="bg-linear-to-r from-accent to-purple-500 bg-clip-text text-transparent ml-0 sm:ml-2 sm:mt-1 inline-block">
+          <span className="mt-4 bg-linear-to-r from-accent to-purple-500 bg-clip-text text-transparent ml-0 sm:ml-2 sm:mt-1 inline-block">
             Adeyinka
           </span>
         </h1>
@@ -30,7 +30,7 @@ export default function HeroContent() {
       </div>
 
       {/* Description */}
-      <p className="max-w-xl text-lg text-muted-foreground leading-relaxed">
+      <p className="max-w-xl text-base md:text-lg text-muted-foreground leading-relaxed">
         I build modern, responsive, and scalable web applications with the React/Next.js and Node.js ecosystem, bringing creative and functional ideas to life on the web.
       </p>
 
